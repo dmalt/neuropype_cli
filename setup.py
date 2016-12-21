5e0af2b242c3ff175b73e2742230d801cc503982
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-12-21 18:12:59 -0500

    Changed setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,25 @@ from setuptools import setup
 
 setup(
     name='neuropype_cli',
-    version='0.1',
-    py_modules=['neuropype'],
-    author='cocolab',
+    version='0.1.1',
+    author='Dmitrii Altukhov',
+    authou_email='dm.altukhov@ya.ru',
     description='command line interface for neuropype_ephy',
     lisence='MIT',
-    zip_sage=False,
+    zip_safe=False,
+    packages=['neuropype_cli'],
     url='https://github.com/dmalt/neuropype_cli.git',
+    dependency_links=['https://github.com/dmalt/neuropype_ephy/tarball/dev#egg=neuropype_ephy'],
     install_requires=[
         'numpy',
         'mne',
         'nipype',
-        'neuropype_ephy',
-        'Click'
+        'Click',
+        'configparser',
+        'neuropype_ephy'
     ],
     entry_points='''
         [console_scripts]
-        neuropype=neuropype:cli
+        neuropype=neuropype_cli.neuropype:cli
     '''
 )
