43eb6de30dcdef74b550b0135ef832e20c4d4fa1
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-12-21 12:01:51 -0500

    Added dependencies to setup.py, changed name to neuropype_cli

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup
 
 setup(
-    name='neuropype',
+    name='neuropype_cli',
     version='0.1',
     py_modules=['neuropype'],
+    author='cocolab',
+    description='command line interface for neuropype_ephy',
+    lisence='MIT',
+    zip_sage=False,
+    url='https://github.com/dmalt/neuropype_cli.git',
     install_requires=[
-        'Click',
+        'numpy',
+        'mne',
+        'nipype',
+        'neuropype_ephy',
+        'Click'
     ],
     entry_points='''
         [console_scripts]
         neuropype=neuropype:cli
-    ''',
+    '''
 )
