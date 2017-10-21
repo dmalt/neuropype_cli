7388c9c63f7f38fe6b5a02ed0125a7ed94089a60
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-10-22 00:39:51 +0300

    test

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,16 @@ import sys
 sys.path.insert(0, os.path.abspath('../neuropype_cli'))
 
 
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):  
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.linalg', 'scipy.signal']  
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
